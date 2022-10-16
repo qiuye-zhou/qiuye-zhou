cffc0cc684ef5910922939290100fd9873fc33b7
qiuye <1302580865@qq.com>
2022-10-16 16:51:57 +0800

    docs

--- config.ts
+++ config.ts
@@ -1,34 +1,19 @@
 export const opensource = {
   active: [
-    'mx-space/kami',
-    'mx-space/mx-server',
-    'mx-space/mx-admin',
-    'mx-space/api-client', 'Innei/bump-version',
   ],
   toys: {
     limit: 3,
     random: true,
     repos: [
-      'reactify-component/react-typewriter',
-      'reactify-component/react-message',
-      'Innei/react-shortcut-guide',
-      'Innei/simple-clock',
-      'Innei/shuffle-article',
-      'Innei/candy',
-      'Innei/pageproxy',
-      'Innei/animate-uri',
-      'Innei/vue-react-wrapper' 
     ],
   },
 }
-export const motto = `<p align=center><strong>お可愛いこと。</strong></p>`
+export const motto = `<p align=center><strong>测试</strong></p>`
 export const github = {
-  name: 'Innei',
+  name: 'qiuye-zhou',
 }
 
 export const mxSpace = {
-  url: 'https://innei.ren',
-  api: 'https://api.innei.ren/v2',
 }
 
 export const timeZone = 'Asia/Shanghai'
