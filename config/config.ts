ac5d6114f9020e0ed43719bc02cfa2823509b591
qiuye <1302580865@qq.com>
2022-10-31 21:13:48 +0800

    change: config

--- config/config.ts
+++ config/config.ts
@@ -28,6 +28,6 @@ export const githubAPIEndPoint = 'https://api.github.com'
 
 export const timeZone = 'Asia/Shanghai'
 
-export const motto = `<p align=center><strong></strong></p>`
+export const motto = `<p align=center><strong>如果你热爱生活就不要浪费时间，因为时间组成了生活。</strong></p>`
 
 export const Interval_time = 24
